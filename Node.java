1bc3c7471322a85bd20968d574c2af47dc320aef
gang-b <106060917+gang-b@users.noreply.github.com>
2022-05-23 22:57:14 +1200

    Update Node.java

--- Node.java
+++ Node.java
@@ -5,11 +5,19 @@ public class Node {
     public double hValue; //distance to the goal
     public double gValue; //distance from start
     public double finalValue; // hValue+ gValue aka Fvalue
+    public int p_index;
 
-    public Node{ int x, int y}{
-        this.x, this.y;
+    //Included an index
+    public Node(int x, int y, int p_index, double hValue, double gValue){
+        this.x = x;
+        this.y = y;
+        this.p_index = p_index;
+        this.hValue = hValue;
+        this.gValue = gValue;
+        this.finalValue = hValue + gValue; // final value is hValue + gValue so we do not need to input it separately.
     }
+    
 
-    //should there be a method for calculating gValue+hvalue ?
+   
 
 }
